edb5c57990e3cbdb0205d3074c70f033062611f1
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2024-01-01 22:12:07 +0100

    update

--- src/main/java/ma/sdia/App1.java
+++ src/main/java/ma/sdia/App1.java
@@ -1,11 +1,16 @@
 package ma.sdia;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+
 public class App1 {
     public static void main(String[] args) {
-        SparkConf conf=new SparkConf().setAppName("Spark Docker").setMaster("spark://localhost:7077");
+        SparkConf conf=new SparkConf().setAppName("Spark Docker").setMaster("spark://spark-master:7077");
         JavaSparkContext sc=new JavaSparkContext(conf);
-        JavaRDD<Integer>rdd1=sc.parallelize(Arrays.asList(12,6,8,9,10));
+        JavaRDD<Integer> rdd1=sc.parallelize(Arrays.asList(12,6,8,9,10));
         rdd1.collect().forEach(System.out::println);
-    }
-
+    }
 }
